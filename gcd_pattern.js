8187b9ed5bc661726938885e3749fd2fd03225d2
ColbyCabrera <gdthyispro@gmail.com>
2025-07-26 23:23:16 -0400

    Refactor examplePatternSteps to use consistent array structure

--- gcd_pattern.js
+++ gcd_pattern.js
@@ -7,15 +7,14 @@ const readline = require("readline").createInterface({
 // each row is an array of strings, where each string is a pattern of X's and O's
 let examplePatternSteps = {
   // Example pattern for 5 X's and 7 O's
-  step1: [["XXXXXOOOOOOO"]],
-  step2: [["XXXXXOO"], ["OOOOO"]],
-  step3: [["XXX"], ["OOO"], ["OO"], ["XX"], ["OO"]],
+  step1: ["XXXXXOOOOOOO"],
+  step2: ["XXXXXOO", "OOOOO"],
+  step3: ["XXX", "OOO", "OO", "XX", "OO"],
   // Then concat the whole patter by getting the first index of each row
 };
 
 // Uses Euclids algorithm to find the values of each step
-// a = b * q + r where // a is the larger number, b is the smaller number, q is the quotient, and r is the remainder
-//
+// a = b * q + r where a is the larger number, b is the smaller number, q is the quotient, and r is the remainder
 function getGCDStep(xCount, oCount) {}
 
 // Given an array of strings, concatenate through the index 0 to length of the array of the strings
